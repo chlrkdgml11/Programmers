d13f7ed11b27e88cb7674b2db6c5a9b690a1bdae
chlrkdgml11 <chlrkdgml11@naver.com>
2023-11-18 18:25:03 +0900

    Marathon

--- Level 1/Marathon.py
+++ Level 1/Marathon.py
@@ -1,12 +1,13 @@
 def solution(participant, competion):
+    
     dict = {}
     sum = 0
-
-    for i in range(len(participant)):
-        dict[participant[i]] = hash(participant[i])
-        sum += hash(participant[i])
-
-    for j in range(len(competion)):
-        sum -= hash(competion[i])
+    
+    for part in participant:
+        dict[hash(part)] = part
+        sum += hash(part)
+        
+    for comp in competion:
+        sum -= hash(comp)
 
     return dict[sum]
