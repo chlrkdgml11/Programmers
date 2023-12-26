c70b6e1e322567f1577e5fb281398da2aed905ea
chlrkdgml11 <chlrkdgml11@naver.com>
2023-12-27 00:33:58 +0900

    Bracket Order

---
+++ Level 2/Bracket.py
@@ -0,0 +1,28 @@
+S = ")()("
+
+def solution(S):
+    list = []
+
+    answer = True
+
+    for s in S:
+        print('s = ', s)
+        print('list = ', list)
+
+        if s == '(':
+            list.append(s)
+
+        else:
+            if len(s) == 0:
+                return False
+            
+            else:
+                list.pop()
+
+    if len(list) != 0:
+        answer = False
+    
+    return answer
+
+print(solution(S))
+        
